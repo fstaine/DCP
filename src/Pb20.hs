afa14e1688fa31f09f849c7e26d37e715f827dcd
Florian Staine <florian.staine@utbm.fr>
2020-06-01 16:03:54 +0200

    [Pb20] Unfold instead of fold

--- src/Pb20.hs
+++ src/Pb20.hs
@@ -2,6 +2,8 @@ module Pb20
     ( run
     ) where
 
+import Data.Maybe as Maybe
+import Data.List as List
 
 -- Pb20
 -- Given two singly linked lists that intersect at some point, find the intersecting node. The lists are non-cyclical.
@@ -16,18 +18,37 @@ run :: IO ()
 run = let
     a = [3, 7, 8, 10]
     b = [99, 1, 8, 10]
-    res = intersectNode a b
+    intersect = intersectList a b
+    intersectNode = Maybe.listToMaybe intersect
   in
-    putStrLn $ show res
+    do
+      putStr "full intersection: "
+      print intersect
+      putStr "Intersection node: "
+      print intersectNode
 
 
+-- Find the intersection list
+intersectList :: [Int] -> [Int] -> [Int]
+intersectList a b = let
+    zipped = zip (reverse a) (reverse b)
+  in
+    reverse $ List.unfoldr intersecting zipped
+
+
+-- Unpack if both elements are the same
+intersecting :: [(Int, Int)] -> Maybe (Int, [(Int, Int)])
+intersecting ((x, y):lst) | x == y = Just (x, lst)
+intersecting _ = Nothing
+
+
+-- Deprecated way to get the intersection node
 intersectNode :: [Int] -> [Int] -> Maybe Int
 intersectNode a b = let
     a' = reverse a
     b' = reverse b
     zipped = zip a' b'
     -- Solution if same value === same node (so also same next values)
-    intersectionFinder = \prev (x, y) -> if x == y then Just x else prev
+    intersectionFinder prev (x, y) = if x == y then Just x else prev
   in
     foldl intersectionFinder Nothing zipped
-
